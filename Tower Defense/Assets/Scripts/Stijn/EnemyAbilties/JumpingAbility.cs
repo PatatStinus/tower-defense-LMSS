34efdafa465d7904570c3c2c6d14bd9bdcf07623
PatatStinus <stijnrpd@gmail.com>
2021-09-30 20:33:09 +0200

    Update: Ik ga het vandaag niet afkrijgen

--- Tower Defense/Assets/Scripts/Stijn/EnemyAbilties/JumpingAbility.cs
+++ Tower Defense/Assets/Scripts/Stijn/EnemyAbilties/JumpingAbility.cs
@@ -6,12 +6,16 @@ public class JumpingAbility : MonoBehaviour
 {
     [HideInInspector] public bool ability = true;
     [SerializeField] private float jumpWidth;
+    [SerializeField] private float jumpHeight;
+    
     private Vector3 jumpTarget;
     private Vector3 orgPos;
     private bool isJumping = false;
     private bool jumpAnim;
     private EnemyMovement movement;
     private float yPos;
+    private float time = 0;
+    private bool down = false;
 
     private void Start()
     {
@@ -31,27 +35,39 @@ public class JumpingAbility : MonoBehaviour
 
     private void Jump()
     {
-        float xPos = Random.Range(EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex].transform.position.x, EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex + 1].transform.position.x);
-        float zPos = Random.Range(EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex].transform.position.z, EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex + 1].transform.position.z);
-        jumpTarget = new Vector3(xPos, transform.position.y, zPos);
-        movement.GetNewWayPoint();
-        jumpAnim = true;
-        yPos = Vector3.Distance(transform.position, jumpTarget) / 2;
-        orgPos = transform.GetChild(0).transform.localPosition;
+        if (movement != null)
+        {
+            float xPos = Random.Range(EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex].transform.position.x, EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex + 1].transform.position.x);
+            float zPos = Random.Range(EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex].transform.position.z, EnemyPathMaking.t_Points[movement.pathIndex][movement.i_waypoitIndex + 1].transform.position.z);
+            jumpTarget = new Vector3(xPos, transform.position.y, zPos);
+            movement.NewTarget(jumpTarget);
+            jumpAnim = true;
+            yPos = Vector3.Distance(transform.position, jumpTarget);
+            orgPos = transform.GetChild(0).transform.localPosition;
+            movement.usingAbility = true;
+        }
     }
 
     private void LerpToPos()
     {
-        /*float distanceToTarget = Mathf.Abs((yPos * 2 - Vector3.Distance(transform.position, jumpTarget)) / (yPos * 2) - 1);*/
-        float time = Mathf.Abs(Vector3.Distance(transform.position, jumpTarget) / (yPos * 2) - 1);
-        if (time >= 1)
-            Mathf.Abs(Vector3.Distance(transform.position, jumpTarget) / (yPos) - 1);
+        if(!down)
+            time += Time.deltaTime / (jumpWidth * yPos * 0.1f);
+        else if(down)
+            time -= Time.deltaTime / (jumpWidth * yPos * 0.1f);
 
-        transform.GetChild(0).transform.localPosition = Vector3.Lerp(orgPos, new Vector3(orgPos.x, orgPos.y + 10, orgPos.z), time);  /*new Vector3(orgPos.x, (-yPos / jumpWidth) * Mathf.Pow(Vector3.Distance(transform.position, jumpTarget) - yPos, 2) + 5, orgPos.z);*/
-        if (time < 0)
+        if (time >= 1)
+            down = true;
+
+        transform.GetChild(0).transform.localPosition = Vector3.Lerp(orgPos, new Vector3(orgPos.x, orgPos.y + yPos / jumpHeight, orgPos.z), time);
+        
+        if(time <= 0)
         {
             jumpAnim = false;
             isJumping = false;
+            movement.GetNewWayPoint();
+            movement.usingAbility = false;
+            time = 0;
+            down = false;
         }
     }
 }
--- Tower Defense/Assets/Scripts/Stijn/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/Stijn/EnemyMovement.cs
@@ -8,17 +8,18 @@ public class EnemyMovement : MonoBehaviour
     [Range(0f, 20f)] public float f_RotateSpeed = 0.5f;
     [Range(0f, 500f)] [SerializeField] private int i_ManaWhenKilled = 10;
 
-    [HideInInspector] public bool isConfused;
-    [HideInInspector] public float divideSpeed = 1;
+    [HideInInspector] public bool isConfused = false;
+    [HideInInspector] public bool usingAbility = false;
     [HideInInspector] public bool isZapped = false;
+    [HideInInspector] public float divideSpeed = 1;
     [HideInInspector] public int pathIndex;
     [HideInInspector] public int i_waypoitIndex = 0;
-    private Transform t_Target;
+    private Vector3 t_Target;
     private Quaternion q_LookAngle;
 
     private void Start()
     {
-        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex]; 
+        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex].position; 
         transform.LookAt(t_Target);
     }
 
@@ -26,7 +27,7 @@ public class EnemyMovement : MonoBehaviour
     {
         MoveTarget();
 
-        if (Vector3.Distance(transform.position, t_Target.position) <= .5f) //If enemy reached target
+        if (Vector3.Distance(transform.position, t_Target) <= .01f && !usingAbility) //If enemy reached target
             GetNewTarget();
     }
 
@@ -59,21 +60,26 @@ public class EnemyMovement : MonoBehaviour
             }
         }
 
-        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex];
+        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex].position;
     }
 
     private void MoveTarget()
     {
-        Vector3 dir = t_Target.position - transform.position;
+        Vector3 dir = t_Target - transform.position;
         transform.Translate(dir.normalized * (f_Speed / divideSpeed) * Time.deltaTime, Space.World); //Move enemy to target
 
-        Vector3 lookDir = t_Target.position - transform.position;
+        Vector3 lookDir = t_Target - transform.position;
         q_LookAngle = Quaternion.LookRotation(lookDir, transform.up); //Get enemy to target rotation
         transform.rotation = Quaternion.Slerp(transform.rotation, q_LookAngle, f_RotateSpeed * Time.deltaTime); //Rotate enemy to target
     }
     public void GetNewWayPoint()
     {
         i_waypoitIndex++;
-        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex];
+        t_Target = EnemyPathMaking.t_Points[pathIndex][i_waypoitIndex].position;
     } 
+
+    public void NewTarget(Vector3 newTarget)
+    {
+        t_Target = newTarget;
+    }
 }
